dc098cb39cbf2c4b9c33eefec04d518f9faea5db
Artur <arturfilippovskij@gmail.com>
2023-08-19 16:41:11 +0300

    first commit

---
+++ js/animation.js
@@ -0,0 +1,34 @@
+const animItems = document.querySelectorAll('.anim-items')
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll)
+	function animOnScroll(params) {
+
+		for(let i = 0; i < animItems.length; i++) {
+			const animItem = animItems[i];
+			const animItemHight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHight / animStart
+
+			if (animItemHight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart
+			}
+
+			if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHight )) {
+				animItem.classList.add('_active')
+			} else {
+				animItem.classList.remove('_active')
+			}
+		}
+	}
+
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+	}
+	animOnScroll()
+}

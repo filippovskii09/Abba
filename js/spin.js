dd3c173237d7a795e6bac50e2169417bae40d718
Artur <arturfilippovskij@gmail.com>
2023-08-21 10:56:08 +0300

    update

---
+++ js/spin.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+	setTimeout(function() {
+	  const preloader = document.querySelector(".preloader");
+	  const content = document.querySelector(".content");
+ 
+	  preloader.style.display = "none";
+	  content.style.display = "block";
+	}, 300);
+ });
+ 
